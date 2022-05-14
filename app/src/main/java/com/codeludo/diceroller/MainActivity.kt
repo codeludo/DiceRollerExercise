5b7309f97f5a9b7c3431bb93a7da65dae4043a4d
Camilo <jcam.qvision@gmail.com>
2022-05-14 18:48:38 -0500

    dice images added

--- app/src/main/java/com/codeludo/diceroller/MainActivity.kt
+++ app/src/main/java/com/codeludo/diceroller/MainActivity.kt
@@ -2,12 +2,12 @@ package com.codeludo.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultText: TextView
+    private lateinit var diceImage: ImageView
     private lateinit var rollButton: Button
     private lateinit var countUpButton: Button
     private lateinit var resetButton: Button
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultText = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
         rollButton = findViewById(R.id.roll_button)
         countUpButton = findViewById(R.id.countUp_button)
         resetButton = findViewById(R.id.reset_button)
@@ -35,22 +35,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun reset() {
-        resultText.text = 0.toString()
+        TODO()
     }
 
     private fun rollDice(){
         // Toast.makeText(this, "button clicked...", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
     }
 
     private fun countUp(){
-        val currentVal = resultText.text.toString()
+        TODO()
+        /*val currentVal = resultText.text.toString()
 
         if(currentVal == "Hello World!"){
             resultText.text = "1"
         }else if(currentVal.toInt() < 6){
             resultText.text = currentVal.toInt().inc().toString()
-        }
+        }*/
     }
 }
