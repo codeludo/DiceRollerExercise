24beab4636691ec35fb0897b152c0ed14b2a5356
Camilo <jcam.qvision@gmail.com>
2022-05-14 22:08:40 -0500

    second dice added

--- app/src/main/java/com/codeludo/diceroller/MainActivity.kt
+++ app/src/main/java/com/codeludo/diceroller/MainActivity.kt
@@ -8,24 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     // best way
-    private lateinit var diceImage: ImageView
+    private lateinit var diceImage1: ImageView
+    private lateinit var diceImage2: ImageView
     private lateinit var rollButton: Button
     private lateinit var countUpButton: Button
     private lateinit var resetButton: Button
-
-    /*
-    // complicate code because is necessary check nullity every time we use it
-    private var diceImage: ImageView? = null
-    private var rollButton: Button? = null
-    private var countUpButton: Button? = null
-    private var resetButton: Button? = null
-    */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         rollButton = findViewById(R.id.roll_button)
         countUpButton = findViewById(R.id.countUp_button)
         resetButton = findViewById(R.id.reset_button)
@@ -48,7 +42,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice(){
-        // Toast.makeText(this, "button clicked...", Toast.LENGTH_SHORT).show()
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun countUp(){
+        TODO()
+    }
+
+    private fun getRandomDiceImage(): Int{
         val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -57,18 +59,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
-
-    }
-
-    private fun countUp(){
-        TODO()
-        /*val currentVal = resultText.text.toString()
-
-        if(currentVal == "Hello World!"){
-            resultText.text = "1"
-        }else if(currentVal.toInt() < 6){
-            resultText.text = currentVal.toInt().inc().toString()
-        }*/
+        return drawableResource
     }
 }
